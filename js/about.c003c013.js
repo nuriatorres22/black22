48ed7ac4d206fed9ba75cd34a94eadf96692e3fb
nuriatorres22 <113595675+nuriatorres22@users.noreply.github.com>
2022-12-15 12:22:43 +0100

    Add files via upload

---
+++ js/about.c003c013.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkpractica2"]=self["webpackChunkpractica2"]||[]).push([[443],{5399:function(t,n,u){u.r(n),u.d(n,{default:function(){return i}});var a=function(){var t=this;t._self._c;return t._m(0)},s=[function(){var t=this,n=t._self._c;return n("div",{staticClass:"about"},[n("h1",[t._v("This is an about page")])])}],c=u(1001),r={},e=(0,c.Z)(r,a,s,!1,null,null,null),i=e.exports}}]);
+//# sourceMappingURL=about.c003c013.js.map
